cf3040f0cb6af0cf8bdbdf59d3618bc9d6175c6c
56KbModem <nick-snel@hotmail.com>
2019-05-16 15:09:21 +0200

    started with writing picam implementation

--- module/pi_cam.py
+++ module/pi_cam.py
@@ -2,8 +2,27 @@ import webcam
 from picamera import PiCamera
 
 
-class PiCam(webcam):
+class PiCam():
     """Class for the PiCam"""
     def __init__(self):
+        self.camera = PiCamera()
+        self.camera.resolution = (1280, 720)
 
-    def 
+    def record(self, time):
+        """
+        this function takes a time in seconds and records to the
+        filename that is specified ...
+        """
+
+        self.camera.start_preview()
+        self.camera.start_recording("testfile.mjpeg", quality=30)
+        self.camera.wait_recording(time)
+        self.camera.stop_recording()
+        self.camera.stop_preview()
+
+
+print("starting camera...")
+my_camera = PiCam()
+my_camera.record(10)
+
+print("done...")
