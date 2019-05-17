89827b77460347622ad408647902bf522ff1bec0
Jesper Obbes <jesper.obbes@student.hu.nl>
2019-05-17 13:35:22 +0200

    Added several new functions, still WIP

--- module/pi_cam.py
+++ module/pi_cam.py
@@ -1,12 +1,31 @@
-import webcam
 from picamera import PiCamera
+from time import sleep
 
 
-class PiCam():
-    """Class for the PiCam"""
-    def __init__(self):
+class PiCam:
+    """
+    Picam class
+    Default resolution = 1280 by 720
+    """
+    def __init__(self, comm: BaseComm):
+        self.comm = comm
+        # self.comm,listen_for([FrameType.tobeadded])
         self.camera = PiCamera()
         self.camera.resolution = (1280, 720)
+        # Warm up camera
+        sleep(2)
+
+    def process(self):
+        while self.comm.has_data():
+            frame = self.comm.get_data()
+
+            if frame.request:
+                continue
+
+            values = frame.get_data()
+
+    def stop(self):
+        self.comm.stop()
 
     def record(self, time):
         """
@@ -20,6 +39,28 @@ class PiCam():
         self.camera.stop_recording()
         self.camera.stop_preview()
 
+    def capture(self):
+        """
+        Function to capture a single frame
+        :return:
+        """
+        self.camera.capture('test.jpg')
+
+    def set_resolution(self, x, y):
+        """
+        Changes the resolution of the PiCamera
+        :param x: amount of pixels for x
+        :param y: amount of pixels for y
+        :return:
+        """
+        self.camera.resolution = (x, y)
+
+    def low_light_capture(self):
+        """
+        Function which enables low lightning capture to take pictures/videos when it dark.
+        :return:
+        """
+        pass
 
 print("starting camera...")
 my_camera = PiCam()
