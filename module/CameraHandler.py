bca17109c14859ec50f19e0d49d7684e23d6a0d2
Brandon Kroes <b.kroes@protonmail.com>
2019-06-13 14:33:54 +0200

    Initialised the camerahandler

---
+++ module/CameraHandler.py
@@ -0,0 +1,17 @@
+class CameraHandler:
+    def __init__(self):
+        """ Should detect which camera's are present"""
+        pass
+
+    def capture(self):
+        """ Instructs a camera to take an image"""
+        pass
+
+    def start_recording(self):
+        """ Instructs a camera to start recording """
+        pass
+
+    def stop_recording(self):
+        """ Instructs a camera to stop recording """
+        pass
+
